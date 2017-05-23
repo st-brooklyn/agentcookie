0caf39b64cb7ac7fa622f697f6663744d9605d06
Brooklyn Steinfeld <brooklyn.steinfeld@outlook.com>
2017-05-23 16:23:08 +0700

    Update botengine.php

--- botengine.php
+++ botengine.php
@@ -21,7 +21,7 @@ fclose($myfile);
 function getReply($message = "") {
   $response = $message . " ";
   if (isset($message, array_keys($botwords))) {
-    $response .= array_values($botwords[$message]);
+    $response .= $botwords[$message];
   }
   else {
     $response .= "I will get back to you ASAP.";
