4382e26171a6ed27e97518dfdc10dcd8fc016914
Brooklyn Steinfeld <brooklyn.steinfeld@outlook.com>
2017-05-23 16:19:16 +0700

    Update botengine.php

--- botengine.php
+++ botengine.php
@@ -32,6 +32,7 @@ function getReply($message = "") {
 
 echo getReply("tour");
 
-//print_r(array_keys($botwords));
-//print_r(array_values($botwords));
+print_r(array_keys($botwords));
+echo "<br />";
+print_r(array_values($botwords));
 ?>
