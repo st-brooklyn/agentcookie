113bf85d2ee5c214f2f3d52e317545bee5f1e9c4
Brooklyn Steinfeld <brooklyn.steinfeld@outlook.com>
2017-05-23 15:47:35 +0700

    Update botengine.php

--- botengine.php
+++ botengine.php
@@ -1,5 +1,6 @@
 <?php
 $botfile = "botdata.txt";
+$delim = '|#|';
 //Open a botdata.txt file
 $myfile = fopen($botfile, "r") or die("Unable to open file!");
 
@@ -9,9 +10,13 @@ $line = "";
 while(!feof($myfile)) {
   $line = fgets($myfile);
   // Add each line into response dictionary
-  
+  $entry = explode($delim, $line);
+  $botwords[$entry[0]] = $entry[1];
   // Output one line until end-of-file
   echo $line . "<br>";
 }
 fclose($myfile);
+
+print_r(array_keys($botwords));
+print_r(array_values($botwords));
 ?>
