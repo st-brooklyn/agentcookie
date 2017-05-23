b1164997166ed1122670ac6bba5dd16111224f6f
Brooklyn Steinfeld <brooklyn.steinfeld@outlook.com>
2017-05-23 16:06:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,15 +21,15 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				//'text' => $text
+				'text' => getReply($text)
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				//'messages' => [$messages],
-				'messages' => getReply([$messages]),
+				'messages' => [$messages],				
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
