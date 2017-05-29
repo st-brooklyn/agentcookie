1efb770254af7b9ff20b02b0ebf6de670f1a9ce0
Brooklyn Steinfeld <brooklyn.steinfeld@outlook.com>
2017-05-29 16:05:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -29,6 +29,8 @@ $access_token = 'dIZf/b/ZabUO0IafFmPxBvcG9xPKQXtGZ6wClV70CCqTwV1TJDT1m58rdm3pko0
 
 // Get POST body content
 $content = file_get_contents('php://input');
+
+error_log(print($content)."/r/n", 3, "bot.log");
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
@@ -50,27 +52,27 @@ if (!is_null($events['events'])) {
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			//$url = 'https://api.line.me/v2/bot/message/reply';
-			//$data = [
-			//	'replyToken' => $replyToken,
-			//	'messages' => [$messages],				
-			//];			
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],				
+			];			
 			
-			//$post = json_encode($data);
-			//$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			//$ch = curl_init($url);
-			//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			//curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			//$result = curl_exec($ch);
-			//curl_close($ch);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 
-			//echo $result . "\r\n";
+			echo $result . "\r\n";
 			
-			echo sendMessage(global $replyToken, global $messages, global $access_token);
+			//echo sendMessage(global $replyToken, global $messages, global $access_token);
 		}
 	}
 }
