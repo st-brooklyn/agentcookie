74ae3a5b985d3b0d37accdcb9da72d3d1e7ab318
Brooklyn Steinfeld <reasontoalive@hotmail.com>
2017-08-24 16:53:36 +0700

    Change logger name

--- verify.php
+++ verify.php
@@ -3,7 +3,7 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 // Create a log channel
-$log = new Logger('Main Bot');
+$log = new Logger('Verify');
 $log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
 
 // $log->error('Bar');
