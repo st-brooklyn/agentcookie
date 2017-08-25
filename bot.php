0e6ef6723192ac238e2dfefaab8a8cd54a1826cb
Brooklyn Steinfeld <reasontoalive@hotmail.com>
2017-08-25 16:28:21 +0700

    rr

--- bot.php
+++ bot.php
@@ -109,7 +109,7 @@ if (!is_null($events['events'])) {
 			//conversation_object
 			$co = ask_ai($text);
 
-			//$log->warning("Raw: " . $co[0]);
+			$log->warning("Raw: " . $co->raw);
 
 			if($co->action->done){
 				// Perform custom action
